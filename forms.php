4932af4fa721fe4e2d1a6d9503a34115e4335455
Michal Hecka <90258289+laralover@users.noreply.github.com>
2021-09-16 08:17:42 +0200

    Create forms.php

---
+++ forms.php
@@ -0,0 +1,19 @@
+<?php
+
+echo <<<E
+
+<h3> dane uzytkownika <h3>
+
+<form method="GET">
+<input type="text" name="name" placeholder="podaj imie"><br>
+<submit type="submit" value="Apply">
+
+</form>
+
+
+E;
+
+
+
+
+?>
